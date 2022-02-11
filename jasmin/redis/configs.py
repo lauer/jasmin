ba95fb3c8c352e9aee583fd5ba220ca7313d8839
Jesper Grann Laursen <jgl@ipvision.dk>
2022-02-11 14:13:38 +0100

    Added support for redis requiring password on connection

--- jasmin/redis/configs.py
+++ jasmin/redis/configs.py
@@ -19,7 +19,7 @@ class RedisForJasminConfig(ConfigFile):
 
         self.host = self._get('redis-client', 'host', '127.0.0.1')
         self.port = self._getint('redis-client', 'port', 6379)
-        self.dbid = self._getint('redis-client', 'dbid', '0')
+        self.dbid = self._getint('redis-client', 'dbid', 0)
         self.password = self._get('redis-client', 'password', None)
         self.poolsize = self._getint('redis-client', 'poolsize', 10)
 
